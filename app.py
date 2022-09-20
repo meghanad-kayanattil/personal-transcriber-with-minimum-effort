1fbdaa122704215e553a447b76ad08df8cd7678b
Meghanad <kayanattilmeghanad@gmail.com>
2022-09-20 23:22:41 +0200

    primary run with simple model

---
+++ app.py
@@ -0,0 +1,56 @@
+from cmath import log
+from threading import Thread
+from turtle import onclick
+import pyaudio
+import streamlit as st
+import queue
+from threading import Thread
+import helper_functions as hf
+
+def callback():
+    st.session_state['control loop'] = False
+    messages.get()
+
+messages = queue.Queue()
+recordings = queue.Queue()
+output = queue.Queue()
+
+st.title("Speech Helper 🎤")
+
+st.write("""
+### A simple speech recognition app
+""")
+
+p=pyaudio.PyAudio()
+list_device = []
+for i in range(p.get_device_count()):
+    list_device.append(p.get_device_info_by_index(i)['name'])
+p.terminate()
+device = st.selectbox(label='Select the corresponding input device', options=list_device)
+for index, d in enumerate(list_device):
+    if d==device:
+        break
+
+st.session_state['control loop'] = True
+
+if st.button(label = 'Record', key = 'start'):
+    messages.put(True)
+    record = Thread(target=hf.record_microphone, args=(messages, index, recordings,))
+    record.start()
+    transcribe = Thread(target=hf.speech_recognition, args=(messages, recordings, output,))
+    transcribe.start()
+    st.write('Recording started with '+ str(device)+' of index = '+str(index))    
+
+    if st.button("Stop recording", key = 'stop_'+str(i), on_click=callback):
+        st.write("Recording stopped")
+
+    # creating a placeholder for the fixed sized textbox
+    logtxtbox1 = st.empty()
+    logtxt = ""
+    while st.session_state['control loop'] == True:
+        logtxt += output.get()
+        logtxtbox1.text_area("Live transcribe: ", logtxt, height = 500)
+        st.session_state['logged text'] = logtxt
+
+if st.button("Show final tanscipt"):
+    st.write(st.session_state['logged text'])
---
+++ helper_functions.py
@@ -0,0 +1,48 @@
+import pyaudio 
+from vosk import Model, KaldiRecognizer
+import time
+import json
+model = Model(model_name="vosk-model-small-en-us-0.15")
+
+
+CHANNELS = 1
+FRAME_RATE = 16000
+RECORD_SECONDS = 10
+AUDIO_FORMAT = pyaudio.paInt16
+SAMPLE_SIZE = 2
+
+def record_microphone(messages, index, recordings, chunk=1024):
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=AUDIO_FORMAT,
+                    channels=CHANNELS,
+                    rate=FRAME_RATE,
+                    input=True,
+                    input_device_index=index,
+                    frames_per_buffer=chunk)
+
+    frames = []
+
+    while not messages.empty():
+        # print("recording working")
+        data = stream.read(chunk)
+        frames.append(data)
+        if len(frames) >= (FRAME_RATE * RECORD_SECONDS) / chunk:
+            recordings.put(frames.copy())
+            frames = []
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+rec = KaldiRecognizer(model, FRAME_RATE)
+rec.SetWords(True)
+
+def speech_recognition(messages, recordings, output):
+    while not messages.empty():
+        frames = recordings.get()
+        rec.AcceptWaveform(b''.join(frames))
+        result = rec.Result()
+        text = json.loads(result)["text"]
+        print("Transcribe function working")
+        output.put(text)
